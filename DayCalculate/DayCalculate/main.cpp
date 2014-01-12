7ccdcd523ca8233628a6fc8adef37852f163b5d3
HW12A062 <HW12A062@HW12A062.(none)>
2014-01-12 21:11:25 +0900

    first commit

---
+++ DayCalculate/DayCalculate/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DayCalculate
+//
+//  Created by HW12A062 on 14/01/12.
+//  Copyright (c) 2014年 HW12A062. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
